1e3e00980db52634ad8b56efb3aa8f0bc0890167
CAVEPleaseDontMakeATouhouGacha <100413910+CAVEPleaseDontMakeATouhouGacha@users.noreply.github.com>
2024-03-29 02:12:57 +0000

    Bullet improvements and sprites

--- entities/bullets/Bullet.cs
+++ entities/bullets/Bullet.cs
@@ -53,6 +53,14 @@ public partial class Bullet : Node2D
 	// A member that records the speed of the Bullet in one variable
 	public float scalarSpeed;
 	
+	// A slow modifier for when the Bullet is in the Delay or Decay state 
+	public const float cSlowModifier = 0.5f;
+	
+	// The original speed of the Bullet
+	public float originalSpeed;
+	// The Delay/Decay speed of the Bullet
+	public float slowSpeed;
+	
 	// Movement angle of the Bullet
 	public float movementAngle;
 	
@@ -63,6 +71,13 @@ public partial class Bullet : Node2D
 	
 	// A timer that when it reaches 0 kills the Bullet
 	public int timeoutTimer;
+	
+	// The number of ticks the Bullet stays in the Delay and Decay state
+	public int delayDecayTimer;
+	
+	// Stay in the Delay and Decay state for 16 ticks or 0.1333 seconds
+	// 32 ticks is 0.266 seconds
+	public const int cDelayDecayLength = 16;
 	
 	
 	public AnimatedSprite2D animatedSprite2D;
@@ -80,6 +95,8 @@ public partial class Bullet : Node2D
 	
 	public void spawn(Vector2 spawnPos, Vector2 dir, float scalarSpeed, int timeout) {
 		
+		this.state = BULLET_STATE_DELAY;
+		
 		this.position.Y = spawnPos.Y;
 		this.position.X = spawnPos.X;
 		this.prevPosition.Y = spawnPos.Y;
@@ -89,13 +106,21 @@ public partial class Bullet : Node2D
 		this.GlobalPosition = this.position;
 
 		
-		this.scalarSpeed = scalarSpeed;
+		
+		
+
+		this.originalSpeed = scalarSpeed;
+		this.slowSpeed = scalarSpeed * cSlowModifier;
+		this.scalarSpeed = this.slowSpeed;
 		
 		this.velocity.Y = dir.Y * scalarSpeed;
 		this.velocity.X = dir.X * scalarSpeed;
 		
 		this.timeoutTimer = timeout;
 		
+		// Set Delay
+		this.delayDecayTimer = cDelayDecayLength;
+			
 		
 		
 	}
@@ -110,21 +135,84 @@ public partial class Bullet : Node2D
 		// Record previous position
 		this.prevPosition = this.position;
 		
+		switch (this.state) {
+			
+			case(BULLET_STATE_DELAY): {
+				
+				
+				// Update Delay Timer
+				this.delayDecayTimer = this.delayDecayTimer - 1;
 		
-		// Update position
+				if (this.delayDecayTimer <= 0) {
+					// The bullet is now airborne
+					this.state = BULLET_STATE_AIRBORNE;
+					
+					// Go into intended speed
+					this.scalarSpeed = this.originalSpeed;
+					this.velocity.Y = direction.Y * scalarSpeed;
+					this.velocity.X = direction.X * scalarSpeed;
+					
+					// Do collision detection
+					
+				}
+				
+				
+				break;
+			}
+			
+			case(BULLET_STATE_AIRBORNE): {
+				
+				
+				// Update timeout
+				this.timeoutTimer = this.timeoutTimer - 1;
+		
+				if (this.timeoutTimer <= 0) {
+					// If we timed out we go to Decay
+					this.state = BULLET_STATE_DECAY;
+					
+					// Slow down Bullet
+					this.scalarSpeed = this.slowSpeed;
+					this.velocity.Y = direction.Y * this.scalarSpeed;
+					this.velocity.X = direction.X * this.scalarSpeed;
+					
+					// Set Decay timer
+					this.delayDecayTimer = cDelayDecayLength;
+				}
+				
+				
+				break;
+			}
+			
+			case(BULLET_STATE_DECAY): {
+				
+				
+				
+				// Update Decay Timer
+				this.delayDecayTimer = this.delayDecayTimer - 1;
+		
+				if (this.delayDecayTimer <= 0) {
+					// Finally free the bullet for real
+					QueueFree();
+			
+				}
+				
+				
+				break;
+			}
+			
+			
+			
+			
+			
+		};
+		
+		// Update position no matter the state
 		this.position.Y = this.position.Y + this.velocity.Y;
 		this.position.X = this.position.X + this.velocity.X; 
 		
-		// Update timeout
-		this.timeoutTimer = this.timeoutTimer - 1;
-		
-		if (this.timeoutTimer <= 0) {
-			
-			QueueFree();
-			
-		}
 		
 		
+		// Write back Global position
 		this.GlobalPosition = this.position;
 	}
 		
